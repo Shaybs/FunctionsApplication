5c4a65ff9bee27cbc016bff0314a0b2e85ca18c8
Shaybs <ucashus@ucl.ac.uk>
2020-04-05 23:49:01 +0100

    Updates to routes, etcetera to reduce errors and debug

--- FrontEnd/application/routes.py
+++ FrontEnd/application/routes.py
@@ -1,9 +1,6 @@
 from flask import abort, render_template, redirect, url_for, request, flash
-from application.forms import RegistrationForm, LoginForm, UpdateAccountForm, CountryForm
-from application.models import Users
-from application import app, db, bcrypt, login_manager
+from application import app, bcrypt
 import requests
-from flask_login import login_user, current_user, logout_user, login_required
 
 #Render the home page
 @app.route('/')
@@ -20,13 +17,12 @@ def about():
 @app.route('/value', methods=['GET', 'POST'])
 def value():
 
-	
 	value = requests.post('https://shaybsapp.azurewebsites.net/api/centralserver?code=qgixZAslpaGvBpqTnMOp2cVBvct6UozfMCQRwuNHIKZhGXTY2utsag==')
 	if value.ok:
 		flash('New Value!')
 		return redirect(url_for('value'))
 
-	return render_template('value.html', title='Functions Application', user=user)
+	return render_template('value.html', title='Functions Application', value=value)
 
 
 
